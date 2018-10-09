a96622c000836457e4d35696473e5d0321ecd5d8
ciaran <ciarankehoe22@gmail.com>
2018-10-09 00:49:56 +0000

    First Commit

---
+++ jquery/script.js
@@ -0,0 +1,118 @@
+
+$(document).ready(function(){
+    $(".top").hide();
+
+    $(".video").click(function(){
+        $(".top").toggle();
+		 
+    });
+});
+
+$(document).ready(function(){
+    $(".top1").hide();
+
+    $(".video1").click(function(){
+        $(".top1").toggle();
+		 
+    });
+});
+
+$(document).ready(function(){
+    $(".top2").hide();
+
+    $(".video2").click(function(){
+        $(".top2").toggle();
+		 
+    });
+});
+
+$(document).ready(function(){
+    $(".top3").hide();
+
+    $(".video3").click(function(){
+        $(".top3").toggle();
+		 
+    });
+});
+
+$(document).ready(function(){
+    $(".top4").hide();
+
+    $(".video4").click(function(){
+        $(".top4").toggle();
+		 
+    });
+});
+
+$(document).ready(function(){
+    $(".top5").hide();
+
+    $(".video5").click(function(){
+        $(".top5").toggle();
+		 
+    });
+});
+
+$(document).ready(function(){
+    $(".top6").hide();
+
+    $(".video6").click(function(){
+        $(".top6").toggle();
+		 
+    });
+});
+
+$(document).ready(function(){
+    $(".top7").hide();
+
+    $(".video7").click(function(){
+        $(".top7").toggle();
+		 
+    });
+});
+
+$(document).ready(function(){
+    $(".top8").hide();
+
+    $(".video8").click(function(){
+        $(".top8").toggle();
+		 
+    });
+});
+
+$(document).ready(function(){
+    $(".top9").hide();
+
+    $(".video9").click(function(){
+        $(".top9").toggle();
+		 
+    });
+});
+
+$(document).ready(function(){
+    $(".top10").hide();
+
+    $(".video10").click(function(){
+        $(".top10").toggle();
+		 
+    });
+});
+
+$(document).ready(function(){
+    $(".top11").hide();
+
+    $(".video11").click(function(){
+        $(".top11").toggle();
+		 
+    });
+});
+
+$(document).ready(function(){
+    $(".top12").hide();
+
+    $(".video12").click(function(){
+        $(".top12").toggle();
+		 
+    });
+});
+
